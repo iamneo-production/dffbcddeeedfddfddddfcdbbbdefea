e72a60d0e3558dd229b97b3264978cc31bbd7da3
Iamneo <techadmin@iamneo.ai>
2023-08-14 14:38:37 +0000

    Backup Commit

--- springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
+++ springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -1,5 +1,7 @@
 package com.examly.springapp;
 
+
+
 import org.testng.annotations.Test;
 import java.net.URL;
 
@@ -12,21 +14,71 @@ import org.testng.annotations.AfterTest;
 
 public class SpringApplicationTests {
 
-    ChromeOptions chromeOptions = new ChromeOptions();
-    WebDriver driver = null;
+	ChromeOptions chromeOptions = new ChromeOptions();
+	WebDriver driver = null;
 
-    @BeforeTest
-    public void beforeTest() throws Exception
-     {
-   // replace seleniumhost and port with correct values
-        driver = new RemoteWebDriver(new URL("http://<seleniumhost:port>/"), chromeOptions);
-        driver.manage().window().maximize();
-    }
+	@BeforeTest
+	public void beforeTest() throws Exception
+	 {
+			driver = new RemoteWebDriver(new URL("http:34.85.242.216.4444"), chromeOptions);
+	}
 
-    @AfterTest
-    public void afterTest() 
-    {
-        driver.quit();
-    }
+
+	@Test
+//Checking the title of iamNeo (Home - iamneo)
+	public void iamNeo() throws InterruptedException 
+	{
+
+		driver.get("http://iamneo.ai");
+
+        // Verify page title
+        String pageTitle = driver.getTitle();
+        Assert.assertEquals(pageTitle, "We are Hiring!", "Page title doesn't match. FAIL.");
+
+	}
+
+	@Test
+//Moving to FACEBOOK
+	public void nextPage() throws InterruptedException 
+	{
+		 	
+driver.get("https://www.facebook.com");
+
+
+
+
+
+
+
+	}
+	@Test
+//Back to iamNeo
+	public void backPage() throws InterruptedException 
+	{
+		 // Navigate back to iamneo.ai
+        driver.navigate().back();
+
+	}
+	@Test
+//Current URL
+public void currentURL() throws InterruptedException 
+{
+		 // Print the current URL
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("Current URL: " + currentURL);
+// Navigate forward
+        driver.navigate().forward();
+
+        // Reload the page
+        driver.navigate().refresh();
+
+
+}
+
+	@AfterTest
+	public void afterTest() 
+	{
+		driver.quit();
+	}
 
 }
